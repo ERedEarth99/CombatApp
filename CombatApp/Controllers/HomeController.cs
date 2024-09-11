51641a75af61e665bf34ba4b8d16f773abb37694
EzraR <EzraR@DESKTOP-QHUN9V2>
2024-09-11 13:43:50 -0500

    Add project files.

---
+++ CombatApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CombatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CombatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var Protag = new Character();
+            ViewBag.Win = true;
+            Protag.HP = 15;
+            Protag.Skillpoints = 10;
+            return View(Protag);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Character Protag)
+        {
+            Protag.Skillpoints += 1;
+            Protag.Level += 1;
+
+            var Enemy = new Character();
+            Enemy.HP = 99;
+            Enemy.Attack = 99;
+            Enemy.Defense = 99;
+            if (Protag.combat(Enemy) == 0)
+            {
+                ViewBag.Win = "WIN";
+                ViewBag.Win = true;
+            }
+            else
+            {
+                ViewBag.Win = "LOSE";
+                ViewBag.Win = false;
+            }
+            return View(Protag);
+
+        }
+
+    }
+}
